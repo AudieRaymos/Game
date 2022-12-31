028cebe7a636e87dfcef4151336a087bf224cd47
Audie Raymos <audriannakraymos@gmail.com>
2022-12-31 07:46:47 -0800

    set functions for gameplay

--- updateCustomProperty.js
+++ updateCustomProperty.js
@@ -1,1 +1,11 @@
-function getCustomProperty
+export function getCustomProperty(elem, prop) {
+    return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0 
+}
+
+export function setCustomProperty(elem, prop, value) {
+    elem.style.setProperty(prop, value)
+}
+
+export function incrementCustomProperty() {
+    setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc)
+}
