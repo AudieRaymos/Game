f52717fce5d2b3b1a57b048f424f49020419a99b
Audie Raymos <audriannakraymos@gmail.com>
2022-12-31 11:23:29 -0800

    working on ground functionality

--- ground.js
+++ ground.js
@@ -1,7 +1,12 @@
 import { incrementCustomProperty } from "./updateCustomProperty.js"
 
-const SPEED = .05
+const SPEED = 0.05
 const groundElems = document.querySelectorAll("[data-ground]")
+
+export function setupGround() {
+    setCustomProperty(groundElems[0], "--left", 0)
+    setCustomProperty(groundElems[1], "--left", 300)
+}
 
 export function updateGround(delta) {
     groundElems.forEach(ground => {
